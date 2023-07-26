795878d086a5cdd5a95a77657c2ea78cf185a596
Tim-Dexter <timothyanyas96@gmail.com>
2023-07-26 15:56:26 -0700

    4-rev_array.c

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,44 +1,38 @@
 #include "main.h"
-
 #include <stdio.h>
 
 /**
-
  * reverse_array - prints reverse array.
-
  * @a: array to be compared.
-
  * @n: size of array.
-
  * Return: reversed array.
-
  */
 
 void reverse_array(int *a, int n)
 
 {
 
-        int swap, begin, end;
+	int swap, begin, end;
 
 
-        begin = 0;
+	begin = 0;
 
-        end = n - 1;
+	end = n - 1;
 
-        while (begin < end)
+	while (begin < end)
 
-        {
+	{
 
-                swap = *(a + begin);
+	swap = *(a + begin);
 
-                *(a + begin) = *(a + end);
+	*(a + begin) = *(a + end);
 
-                *(a + end) = swap;
+	*(a + end) = swap;
 
-                begin++;
+	begin++;
 
-                end--;
+	end--;
 
-        }
+	}
 
 }
